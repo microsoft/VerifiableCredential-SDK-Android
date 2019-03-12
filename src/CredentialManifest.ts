5022700b104ce3b2c5e57b8fe789dee70130ab41
Sydney Morton <symorton@microsoft.com>
2019-03-11 17:00:10 -0700

    changed implementation of create for credentialManifest.

--- src/CredentialManifest.ts
+++ src/CredentialManifest.ts
@@ -4,20 +4,20 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { CredentialInput, DataInput } from './types';
+
+/**
+ * context for credentialManifest
+ */
+const context = 'https://identity.foundation/schemas/credentials';
+
+/**
+ * type for credentialManifest
+ */
+const type = 'CredentialManifest';
 /**
  * Interface defining methods and properties for a ClaimManifest object.
  */
 export default class CredentialManifest {
-
-  /**
-   * context for credentialManifest
-   */
-  public readonly context = 'https://identity.foundation/schemas/credentials';
-
-  /**
-   * type for credentialManifest
-   */
-  public readonly type = 'CredentialManifest';
 
   /**
    * Name of the credential
@@ -35,25 +35,33 @@ export default class CredentialManifest {
   public inputs: Array<CredentialInput | DataInput>;
 
   /**
-   * issuer options for the style of the manifest in the UI.
+   * issuer options for things such as the style of the manifest in the UI.
    */
   public issuerOptions: any;
 
   /**
-   * Contructs an instance of the CredentialManifest class
+   * Contructs an instance of the CredentialManifest class from a well-formed credential manifest JSON object.
    */
-  constructor (credential: string, preconditions: any, inputs: any, issuerOptions: any) {
-    this.credential = credential;
-    this.preconditions = preconditions;
-    this.inputs = inputs;
-    this.issuerOptions = issuerOptions;
+  constructor (credentialManifest: any) {
+    this.credential = credentialManifest.credential;
+    this.preconditions = credentialManifest.preconditions;
+    this.inputs = credentialManifest.inputs;
+    this.issuerOptions = credentialManifest.issuerOptions;
   }
 
   /**
    * Creates a new instance of the CredentialManifest class.
    */
-  public static create (parameters: any) {
-    return new CredentialManifest(parameters.credential, parameters.preconditions, parameters.inputs, parameters.issuerOptions);
+  public static create (credential: string, preconditions: any, inputs: any, issuerOptions: any) {
+    const manifest = {
+      '@context': context,
+      '@type': type,
+      'credential': credential,
+      'preconditions': preconditions,
+      'inputs': inputs,
+      'issuer_options': issuerOptions
+    };
+    return new CredentialManifest(manifest);
   }
 
   /**
@@ -61,14 +69,35 @@ export default class CredentialManifest {
    */
   public form () {
     const manifest = {
-      '@context': this.context,
-      '@type': this.type,
+      '@context': context,
+      '@type': type,
       'credential': this.credential,
       'preconditions': this.preconditions,
       'inputs': this.inputs,
       'issuer_options': this.issuerOptions
     };
     return manifest;
+  }
+
+  /**
+   * Get the input properties of the manifest
+   */
+  public getInputProperties () {
+    return this.inputs;
+  }
+
+  /**
+   * Get the display properties of the manifest.
+   */
+  public getDisplayProperties () {
+    return this.issuerOptions.style;
+  }
+
+  /**
+   * Get the group expressions of the manifest
+   */
+  public getGroupExpressions () {
+    return this.preconditions.groups;
   }
 
 }
