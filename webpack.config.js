402df9debb25c6aab26f5de4ae185260525cc749
Logan Girvin <logirvin@microsoft.com>
2019-06-10 09:21:28 -0700

    import { UserAgent } ...

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname),
-    library: 'useragentSdk',
+    library: 'UserAgent',
     libraryTarget: 'commonjs'
   },
   resolve: {
