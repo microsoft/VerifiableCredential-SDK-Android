0a75f7bee31ec71bd1b0b48214409e5ee1f53422
Logan Girvin <logirvin@microsoft.com>
2019-08-26 12:09:44 -0700

    This was not fully implemented.

--- kotlin/useragent-sdk/src/commonMain/kotlin/com/microsoft/useragentSdk/crypto/models/webCryptoApi/EcKeyGenParams.kt
+++ kotlin/useragent-sdk/src/commonMain/kotlin/com/microsoft/useragentSdk/crypto/models/webCryptoApi/EcKeyGenParams.kt
@@ -1,4 +1,4 @@
 package com.microsoft.useragentSdk.crypto.models.webCryptoApi
 
-class EcKeyGenParams(name: String, val namedCurve: String) {
+class EcKeyGenParams(name: String, val namedCurve: String, additionalParams: Map<String, Any> = emptyMap()): Algorithm(name, additionalParams) {
 }
