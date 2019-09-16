2246d75041077cdc5d000bc24544a8d3a1a868f6
Sydney <symorton@microsoft.com>
2019-09-16 12:15:28 -0700

    stubbed out JwsToken.

--- kotlin/useragent-sdk/src/commonMain/kotlin/com/microsoft/did/sdk/crypto/protocols/jose/jws/JwsToken.kt
+++ kotlin/useragent-sdk/src/commonMain/kotlin/com/microsoft/did/sdk/crypto/protocols/jose/jws/JwsToken.kt
@@ -9,18 +9,33 @@ class JwsToken {
     /**
      * Serialize a JWS token object from token.
      */
-    fun serialize () {}
+    fun serialize () {
+        TODO("not implemented")
+    }
 
     /**
      * Deserialize a JWS token object
      */
-    fun deserialize() {}
+    fun deserialize() {
+        TODO("not implemented")
+    }
 
     /**
      * Signs contents using the given private key in JWK format
      * @param payload to sign
      * @param signingKeyReference reference to signing key
      */
-    fun sign(payload: String, signingKeyReference: String) {}
+    fun sign(payload: String, signingKeyReference: String) {
+        TODO("not implemented")
+    }
+
+    /**
+     *Verify the JWS signature
+     * @param validationKeys Public JWK key to validate the signature.
+     * @param
+     */
+    fun verify() {
+        TODO("not implemented")
+    }
 
 }
