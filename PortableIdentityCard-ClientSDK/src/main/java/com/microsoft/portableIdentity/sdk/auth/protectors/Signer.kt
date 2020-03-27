668764e9212f3b2c091070581570c1e8ae9d86a2
Andreas Mikolajewski <anmikola@microsoft.com>
2020-03-27 13:32:43 -0700

    Pass defaultSignatureReference to Signer

--- PortableIdentityCard-ClientSDK/src/main/java/com/microsoft/portableIdentity/sdk/auth/protectors/Signer.kt
+++ PortableIdentityCard-ClientSDK/src/main/java/com/microsoft/portableIdentity/sdk/auth/protectors/Signer.kt
@@ -3,12 +3,14 @@ package com.microsoft.portableIdentity.sdk.auth.protectors
 import com.microsoft.portableIdentity.sdk.crypto.CryptoOperations
 import com.microsoft.portableIdentity.sdk.crypto.protocols.jose.jws.JwsToken
 import javax.inject.Inject
+import javax.inject.Named
 
 /**
  * Class that can protect some content by signing.
  */
 class Signer @Inject constructor(
-    private val cryptoOperations: CryptoOperations
+    private val cryptoOperations: CryptoOperations,
+    @Named("signatureKeyReference") private val signatureKeyReference: String
 ) {
 
     /**
@@ -20,7 +22,7 @@ class Signer @Inject constructor(
      *
      * @return JwsToken
      */
-    fun sign(payload: String, keyReference: String = "defaultSigningKey", additionalHeaders: Map<String, String> = emptyMap()): JwsToken {
+    fun sign(payload: String, keyReference: String = signatureKeyReference, additionalHeaders: Map<String, String> = emptyMap()): JwsToken {
         val token = JwsToken(payload)
         token.sign(keyReference, cryptoOperations, additionalHeaders)
         return token
