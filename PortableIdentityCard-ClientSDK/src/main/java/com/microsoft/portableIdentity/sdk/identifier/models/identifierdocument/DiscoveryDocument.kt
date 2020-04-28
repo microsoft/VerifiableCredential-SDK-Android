d8f5090062358bcaaddf1e6b9ad2cb9de8855b49
nithyaganeshng <nithyagansh@gmail.com>
2020-04-28 11:47:11 -0700

    Reverting temporary changes made to parse did document coming from discovery service

--- PortableIdentityCard-ClientSDK/src/main/java/com/microsoft/portableIdentity/sdk/identifier/models/identifierdocument/DiscoveryDocument.kt
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved
-
-package com.microsoft.portableIdentity.sdk.identifier.models.identifierdocument
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DiscoveryDocument (val document: IdentifierResponse)
