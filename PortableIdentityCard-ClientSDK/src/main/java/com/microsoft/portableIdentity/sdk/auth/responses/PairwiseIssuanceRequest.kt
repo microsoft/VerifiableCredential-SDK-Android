2a188bdae052907c9d4af41b21dd8d4a177f3596
sydneymorton <symorton@microsoft.com>
2020-05-07 14:42:03 -0400

    PairwiseIssuanceException

--- PortableIdentityCard-ClientSDK/src/main/java/com/microsoft/portableIdentity/sdk/auth/responses/PairwiseIssuanceRequest.kt
+++ PortableIdentityCard-ClientSDK/src/main/java/com/microsoft/portableIdentity/sdk/auth/responses/PairwiseIssuanceRequest.kt
@@ -1,6 +1,8 @@
 package com.microsoft.portableIdentity.sdk.auth.responses
 
 import com.microsoft.portableIdentity.sdk.cards.verifiableCredential.VerifiableCredential
+import com.microsoft.portableIdentity.sdk.utilities.controlflow.PairwiseIssuanceException
 
-class PairwiseIssuanceRequest(override val audience: String, val verifiableCredential: VerifiableCredential) : ServiceRequest {
+class PairwiseIssuanceRequest(val verifiableCredential: VerifiableCredential) : ServiceRequest {
+    override val audience: String = verifiableCredential.contents.vc.exchangeService?.id ?: throw PairwiseIssuanceException("No Exchange Service in Verifiable Credential.")
 }
