b4e125361a23bd7ed8942878b512ea57d603c7ef
Andreas Mikolajewski <anmikola@microsoft.com>
2020-06-06 22:16:39 -0700

    Suppress room primary key warning

--- sdk/src/main/java/com/microsoft/did/sdk/cards/PortableIdentityCard.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/cards/PortableIdentityCard.kt
@@ -8,6 +8,7 @@ package com.microsoft.did.sdk.cards
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.RoomWarnings
 import com.microsoft.did.sdk.auth.models.contracts.display.DisplayContract
 import com.microsoft.did.sdk.cards.verifiableCredential.VerifiableCredential
 import com.microsoft.did.sdk.identifier.Identifier
@@ -18,6 +19,7 @@ import kotlinx.serialization.Serializable
  */
 @Entity
 @Serializable
+@SuppressWarnings(RoomWarnings.PRIMARY_KEY_FROM_EMBEDDED_IS_DROPPED)
 data class PortableIdentityCard (
 
     // id of the prime Verifiable Credential
