9795d1916c406efaf8e2a500d4f5f8cc2efd43d4
nithyaganeshng <nithyagansh@gmail.com>
2020-10-15 11:13:42 -0700

    Removed unused imports

--- sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/DomainLinkageCredentialValidator.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/DomainLinkageCredentialValidator.kt
@@ -2,8 +2,6 @@
 
 package com.microsoft.did.sdk.credential.service.validators
 
-import com.microsoft.did.sdk.credential.service.models.dnsBinding.DomainLinkageCredential
-
 interface DomainLinkageCredentialValidator {
 
     suspend fun validate(domainLinkageCredentialJwt: String, rpDid: String, rpDomain: String): Boolean
