c4b1d80cbf93ab6d1f34be11d3bf95beba389696
nithyaganeshng <nithyagansh@gmail.com>
2020-10-16 14:48:03 -0700

    added an import statement

--- sdk/src/main/java/com/microsoft/did/sdk/di/UserAgentInterceptor.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/di/UserAgentInterceptor.kt
@@ -3,13 +3,14 @@
 package com.microsoft.did.sdk.di
 
 import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
 import okhttp3.Response
 import java.io.IOException
 
 class UserAgentInterceptor(private val userAgentInfo: String) : Interceptor {
 
     @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain): Response {
+    override fun intercept(chain: Chain): Response {
         val originalRequest = chain.request()
         val requestWithUserAgentInfo = originalRequest.newBuilder()
             .header("User-Agent", userAgentInfo)
