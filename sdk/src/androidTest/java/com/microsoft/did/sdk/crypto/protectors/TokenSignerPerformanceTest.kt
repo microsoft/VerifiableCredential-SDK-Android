39bab5ca46bf1afcb049aa591df3e2fe87a33821
nithyaganeshng <nithyagansh@gmail.com>
2020-11-30 09:13:32 -0800

    Fixed mocking issue in test

--- sdk/src/androidTest/java/com/microsoft/did/sdk/crypto/protectors/TokenSignerPerformanceTest.kt
+++ sdk/src/androidTest/java/com/microsoft/did/sdk/crypto/protectors/TokenSignerPerformanceTest.kt
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-package com.microsoft.did.sdk.crypto.plugins
+package com.microsoft.did.sdk.crypto.protectors
 
 import android.content.Context
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -12,35 +12,34 @@ import com.microsoft.did.sdk.credential.service.protectors.TokenSigner
 import com.microsoft.did.sdk.credential.service.validators.JwtValidator
 import com.microsoft.did.sdk.crypto.CryptoOperations
 import com.microsoft.did.sdk.crypto.keyStore.AndroidKeyStore
-import com.microsoft.did.sdk.crypto.keys.TestKeys
 import com.microsoft.did.sdk.crypto.keys.ellipticCurve.EllipticCurvePairwiseKey
-import com.microsoft.did.sdk.crypto.keys.ellipticCurve.EllipticCurvePublicKey
 import com.microsoft.did.sdk.crypto.models.Sha
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.*
+import com.microsoft.did.sdk.crypto.models.webCryptoApi.CryptoKeyPair
+import com.microsoft.did.sdk.crypto.models.webCryptoApi.JsonWebKey
+import com.microsoft.did.sdk.crypto.models.webCryptoApi.KeyUsage
+import com.microsoft.did.sdk.crypto.models.webCryptoApi.W3cCryptoApiConstants
 import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.EcKeyGenParams
+import com.microsoft.did.sdk.crypto.plugins.AndroidSubtle
+import com.microsoft.did.sdk.crypto.plugins.EllipticCurveSubtleCrypto
 import com.microsoft.did.sdk.crypto.protocols.jose.jws.JwsToken
 import com.microsoft.did.sdk.datasource.db.SdkDatabase
 import com.microsoft.did.sdk.datasource.db.dao.IdentifierDao
-import com.microsoft.did.sdk.datasource.network.apis.ApiProvider
+import com.microsoft.did.sdk.datasource.repository.IdentifierRepository
 import com.microsoft.did.sdk.di.defaultTestSerializer
 import com.microsoft.did.sdk.identifier.IdentifierCreator
 import com.microsoft.did.sdk.identifier.SidetreePayloadProcessor
 import com.microsoft.did.sdk.identifier.models.Identifier
-import com.microsoft.did.sdk.identifier.resolvers.Resolver
-import kotlinx.serialization.json.Json
-import org.junit.Test
-import org.junit.runner.RunWith
-import com.microsoft.did.sdk.util.controlflow.Result
-import kotlinx.serialization.decodeFromString
-import com.microsoft.did.sdk.datasource.repository.IdentifierRepository
-import com.microsoft.did.sdk.identifier.models.identifierdocument.IdentifierDocument
 import com.microsoft.did.sdk.identifier.models.identifierdocument.IdentifierDocumentPublicKey
 import com.microsoft.did.sdk.identifier.models.identifierdocument.IdentifierResponse
+import com.microsoft.did.sdk.identifier.resolvers.Resolver
+import com.microsoft.did.sdk.util.controlflow.Result
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import java.lang.Exception
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class TokenSignerPerformanceTest {
@@ -61,6 +60,7 @@ class TokenSignerPerformanceTest {
         .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
         .map(charPool::get)
         .joinToString("")
+    private val resolver: Resolver
 
     init {
         println("PerfTest->(${getTestName()}) - Start init")
@@ -84,19 +84,19 @@ class TokenSignerPerformanceTest {
         val sidetreePayloadProcessor = SidetreePayloadProcessor(defaultTestSerializer)
         val identifierDao: IdentifierDao = mockk()
         val sdkDatabase: SdkDatabase = mockk()
-        val apiProvider: ApiProvider = mockk()
         every { sdkDatabase.identifierDao() } returns identifierDao
-        identifierRepository = IdentifierRepository(sdkDatabase, apiProvider)
+        identifierRepository = mockk()
         identifierCreator = IdentifierCreator(cryptoOperations, sidetreePayloadProcessor)
         val result: Result<Identifier> = identifierCreator.create("ION")
         if (result !is Result.Success) {
             throw Exception("Could not create identifier")
         }
         identifier = result.payload
-        val resolver = setUpResolver(identifier, publicKey)
+        resolver = Resolver("https://beta.discover.did.microsoft.com/1.0/identifiers", identifierRepository)
 
         signer = TokenSigner(cryptoOperations, serializer)
         validator = JwtValidator(cryptoOperations, resolver, defaultTestSerializer)
+        setUpResolver(identifier, publicKey)
 
         println("PerfTest->(${getTestName()}) - End init: ${timer(startTime)}")
     }
@@ -114,11 +114,9 @@ class TokenSignerPerformanceTest {
         return (timing / 1000).toString()
     }
 
-    private fun setUpResolver(identifier: Identifier, publicKey: JsonWebKey): Resolver {
+    private fun setUpResolver(identifier: Identifier, publicKey: JsonWebKey) {
         println("PerfTest->(${getTestName()}) - id: ${identifier.id}")
         println("PerfTest->(${getTestName()}) - kid: ${identifier.signatureKeyReference}")
-        val resolver = Resolver("https://beta.discover.did.microsoft.com/1.0/identifiers", identifierRepository)
-        /*
         val expectedIdentifierDocumentString =
             """{"@context":"https://www.w3.org/ns/did-resolution/v1",
                 |"didDocument":{"id": "${identifier.id}" ,
@@ -128,7 +126,7 @@ class TokenSignerPerformanceTest {
                 |"authentication":["#${identifier.signatureKeyReference}"]},"methodMetadata":{"operationPublicKeys":[{"id":"#${identifier.signatureKeyReference}","controller":"","type":"EcdsaSecp256k1VerificationKey2019","publicKeyJwk":{"kty":"EC","crv":"secp256k1","x":"hA3EaZjvgGpi1AnWmSEzPhheI55MzhdwDUtXcAS36Fk","y":"E44pJp7_md6Z6-yDngOK9LYaW_LTC8BXgSVMQCZeNrA"}}],"recoveryKey":{"kty":"EC","crv":"secp256k1","x":"MDa8v7M4-DN-Vy6ZYYlPYnPtwtTMjvE9H3-wXX2VJKc","y":"BCpmR6pekJvBqEbpH4LpnuLwSJLGdrNPr3AhWIGmbX4"}},"resolverMetadata":{"driverId":"did:ion","driver":"HttpDriver","retrieved":"2020-05-31T09:20:41.162Z","duration":"118.4098ms"}}""".trimMargin()
         val expectedIdentifierResponse =
             defaultTestSerializer.decodeFromString(IdentifierResponse.serializer(), expectedIdentifierDocumentString)
-        */
+
         val key = listOf(
             IdentifierDocumentPublicKey(
                 id = identifier.signatureKeyReference,
@@ -136,10 +134,7 @@ class TokenSignerPerformanceTest {
                 publicKeyJwk = publicKey
             )
         )
-        //coEvery { identifierRepository.resolveIdentifier("https://beta.discover.did.microsoft.com/1.0/identifiers", identifier.id) } returns Result.Success(expectedIdentifierResponse)
-        coEvery { resolver.resolve(identifier.id) } returns Result.Success(IdentifierDocument(key, identifier.id))
-
-        return resolver
+        coEvery { identifierRepository.resolveIdentifier("https://beta.discover.did.microsoft.com/1.0/identifiers", identifier.id) } returns Result.Success(expectedIdentifierResponse)
     }
 
     @Test
@@ -153,15 +148,15 @@ class TokenSignerPerformanceTest {
 
             println("PerfTest->(${getTestName()}) in  μs - ${loop}: Start deserialize: 0")
             startTime = getStartTime()
-            JwsToken.deserialize(signedPayload, defaultTestSerializer)
+            val signature = JwsToken.deserialize(signedPayload, defaultTestSerializer)
             println("PerfTest->(${getTestName()}) in  μs - ${loop}: End deserialize: ${timer(startTime)}")
 
             runBlocking {
                 println("PerfTest->(${getTestName()}) in  μs - ${loop}: Start verify: 0")
                 startTime = getStartTime()
-                //val response = identifierRepository.resolveIdentifier("https://beta.discover.did.microsoft.com/1.0/identifiers", identifier.id)
+                val response =  resolver.resolve(identifier.id)
 
-                //val actualValidationResult = validator.verifySignature(signature)
+                val actualValidationResult = validator.verifySignature(signature)
                 println("PerfTest->(${getTestName()}) in  μs - ${loop}: End verify: ${timer(startTime)}")
             }
         }
