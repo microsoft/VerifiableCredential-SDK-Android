73919659490642905d26edda9bef9a997700813c
Andreas Mikolajewski <anmikola@microsoft.com>
2021-01-27 02:13:50 +0100

    Refactor Secp256k1 Provider

--- sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/Secp256k1Provider.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/Secp256k1Provider.kt
@@ -2,34 +2,17 @@
 
 package com.microsoft.did.sdk.crypto.plugins
 
-import android.util.Base64
-import com.microsoft.did.sdk.crypto.models.Sha
 import com.microsoft.did.sdk.crypto.models.webCryptoApi.CryptoKey
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.CryptoKeyPair
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.JsonWebKey
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.KeyFormat
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.KeyType
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.KeyUsage
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.SubtleCrypto
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.W3cCryptoApiConstants
 import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.Algorithm
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.EcKeyGenParams
 import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.EcdsaParams
-import com.microsoft.did.sdk.crypto.plugins.subtleCrypto.Provider
 import com.microsoft.did.sdk.util.Constants.SECP256K1_CURVE_NAME_EC
-import com.microsoft.did.sdk.util.controlflow.AlgorithmException
 import com.microsoft.did.sdk.util.controlflow.SignatureException
 import com.microsoft.did.sdk.util.convertSignedToUnsignedByteArray
-import com.microsoft.did.sdk.util.generatePublicKeyFromPrivateKey
-import com.microsoft.did.sdk.util.log.SdkLog
-import com.microsoft.did.sdk.util.publicToXY
-import com.microsoft.did.sdk.util.stringToByteArray
 import org.spongycastle.asn1.ASN1InputStream
 import org.spongycastle.asn1.ASN1Integer
 import org.spongycastle.asn1.DERSequenceGenerator
 import org.spongycastle.asn1.DLSequence
 import org.spongycastle.crypto.params.ECDomainParameters
-import org.spongycastle.crypto.params.ECPrivateKeyParameters
 import org.spongycastle.crypto.params.ECPublicKeyParameters
 import org.spongycastle.crypto.signers.ECDSASigner
 import org.spongycastle.jce.ECNamedCurveTable
@@ -37,97 +20,57 @@ import org.spongycastle.jce.provider.BouncyCastleProvider
 import java.io.ByteArrayOutputStream
 import java.math.BigInteger
 import java.security.*
-import java.util.Locale
 import com.microsoft.did.sdk.crypto.keyStore.KeyStore
 
-class Secp256k1Provider : Provider() {
+class Secp256k1Provider {
     init {
         Security.insertProviderAt(BouncyCastleProvider(), Security.getProviders().size + 1)
     }
 
-    data class Secp256k1Handle(val alias: String, val data: ByteArray)
-
-    override val name: String = "ECDSA"
-    override val privateKeyUsage: Set<KeyUsage> = setOf(KeyUsage.Sign)
-    override val publicKeyUsage: Set<KeyUsage> = setOf(KeyUsage.Verify)
-    override val symmetricKeyUsage: Set<KeyUsage>? = null
-
-    override fun onGenerateKeyPair(
-        algorithm: Algorithm,
-        extractable: Boolean,
-        keyUsages: Set<KeyUsage>
-    ): CryptoKeyPair {
-        val random = SecureRandom()
-        val secret = ByteArray(32)
-        random.nextBytes(secret)
-
-        val publicKey = generatePublicKeyFromPrivateKey(secret)
-
-        val signAlgorithm = EcdsaParams(
-            hash = algorithm.additionalParams["hash"] as? Algorithm ?: Sha.SHA256.algorithm,
-            additionalParams = mapOf(
-                "namedCurve" to W3cCryptoApiConstants.Secp256k1.value
-            )
-        )
-
-        return CryptoKeyPair(
-            privateKey = CryptoKey(
-                KeyType.Private,
-                extractable,
-                signAlgorithm,
-                keyUsages.toList(),
-                Secp256k1Handle("", secret)
-            ),
-            publicKey = CryptoKey(
-                KeyType.Public,
-                true,
-                signAlgorithm,
-                publicKeyUsage.toList(),
-                Secp256k1Handle("", publicKey)
-            )
-        )
+    companion object {
+        private const val KEY_ALGORITHM = "EC"
+        private const val KEY_PROVIDER = "SC"
+        private const val SIGNATURE_ALGORITHM = "SHA256WITHPLAIN-ECDSA"
+        private const val PROVIDER = "SC"
     }
 
-    override fun checkGenerateKeyParams(algorithm: Algorithm) {
-        val keyGenParams = algorithm as? EcKeyGenParams ?: throw AlgorithmException("EcKeyGenParams expected as algorithm")
-        if (keyGenParams.namedCurve.toUpperCase(Locale.ROOT) != W3cCryptoApiConstants.Secp256k1.value.toUpperCase(Locale.ROOT) &&
-            keyGenParams.namedCurve.toUpperCase(Locale.ROOT) != W3cCryptoApiConstants.Secp256k1.name.toUpperCase(Locale.ROOT)
-        ) {
-            throw AlgorithmException("The curve ${keyGenParams.namedCurve} is not supported by Secp256k1Provider")
-        }
+    fun onGenerateKeyPair(): KeyPair {
+        val keyGen = KeyPairGenerator.getInstance(KEY_ALGORITHM, KEY_PROVIDER)
+        keyGen.initialize(ECNamedCurveTable.getParameterSpec(SECP256K1_CURVE_NAME_EC))
+        return keyGen.genKeyPair()
     }
 
-    override fun onSign(data: ByteArray, keyStore: KeyStore, keyAlias: String): ByteArray {
-//        val keyData = (key.handle as Secp256k1Handle).data
-//        val ecAlgorithm = algorithm as EcdsaParams
-//        val hashedData = subtleCryptoSha.digest(ecAlgorithm.hash, data)
-//        if (hashedData.size != 32) {
-//            throw SignatureException("Data must be 32 bytes")
-//        }
-//
-//        val signingSigner = ECDSASigner()
-//        val ecDomainParameters = createCurveParameters()
-//        val privateKeyParams = ECPrivateKeyParameters(BigInteger(1, keyData), ecDomainParameters)
-//        signingSigner.init(true, privateKeyParams)
-//
-//        val signature = signingSigner.generateSignature(hashedData)
-//        val oldSig = convertSignatureToUnsignedByteArray(signature)
-//
-//        // New algo
-//
-//        val keySpec = org.spongycastle.jce.spec.ECPrivateKeySpec(BigInteger(1, keyData), ECNamedCurveTable.getParameterSpec(SECP256K1_CURVE_NAME_EC))
-//        val ecRestoredKey = KeyFactory.getInstance("EC", "SC").generatePrivate(keySpec)
-
-
-        val signer = Signature.getInstance("SHA256WITHPLAIN-ECDSA", "SC")
+    fun onSign(data: ByteArray, keyStore: KeyStore, keyId: String): ByteArray {
+        val signingKey = keyStore.getKey<PrivateKey>(keyId)
+        val signer = Signature.getInstance(SIGNATURE_ALGORITHM, PROVIDER)
                 .apply {
-                    initSign(ecRestoredKey)
+                    initSign(signingKey)
                     update(data)
                 }
-        val newSig = signer.sign()
-        val sigEqual = newSig.contentEquals(oldSig)
+        return signer.sign()
+    }
 
-        return newSig
+    override fun onVerify(algorithm: Algorithm, key: CryptoKey, signature: ByteArray, data: ByteArray): Boolean {
+        if (signature.size > 64) {
+            throw SignatureException("Signature is not in R|S format")
+        }
+        val keyData = (key.handle as Secp256k1Handle).data
+        val ecAlgorithm = algorithm as EcdsaParams
+        val hashedData = subtleCryptoSha.digest(ecAlgorithm.hash, data)
+        if (hashedData.size != 32) {
+            throw SignatureException("Data must be 32 bytes")
+        }
+
+        val verifySigner = ECDSASigner()
+        val ecDomainParameters = createCurveParameters()
+        val publicKeyParams = ECPublicKeyParameters(ecDomainParameters.curve.decodePoint(keyData), ecDomainParameters)
+        verifySigner.init(false, publicKeyParams)
+
+        return verifySigner.verifySignature(
+            hashedData,
+            BigInteger(1, signature.sliceArray(0 until signature.size / 2)),
+            BigInteger(1, signature.sliceArray(signature.size / 2 until signature.size))
+        )
     }
 
     // NEW CODE
@@ -168,113 +111,6 @@ class Secp256k1Provider : Provider() {
         val r = seq.getObjectAt(0) as ASN1Integer
         val s = seq.getObjectAt(1) as ASN1Integer
         return Pair(r.positiveValue, s.positiveValue)
-    }
-
-    override fun onVerify(algorithm: Algorithm, key: CryptoKey, signature: ByteArray, data: ByteArray): Boolean {
-        if (signature.size > 64) {
-            throw SignatureException("Signature is not in R|S format")
-        }
-        val keyData = (key.handle as Secp256k1Handle).data
-        val ecAlgorithm = algorithm as EcdsaParams
-        val hashedData = subtleCryptoSha.digest(ecAlgorithm.hash, data)
-        if (hashedData.size != 32) {
-            throw SignatureException("Data must be 32 bytes")
-        }
-
-        val verifySigner = ECDSASigner()
-        val ecDomainParameters = createCurveParameters()
-        val publicKeyParams = ECPublicKeyParameters(ecDomainParameters.curve.decodePoint(keyData), ecDomainParameters)
-        verifySigner.init(false, publicKeyParams)
-
-        return verifySigner.verifySignature(
-            hashedData,
-            BigInteger(1, signature.sliceArray(0 until signature.size / 2)),
-            BigInteger(1, signature.sliceArray(signature.size / 2 until signature.size))
-        )
-    }
-
-    override fun onImportKey(
-        format: KeyFormat,
-        keyData: JsonWebKey,
-        algorithm: Algorithm,
-        extractable: Boolean,
-        keyUsages: Set<KeyUsage>
-    ): CryptoKey {
-        val alias = keyData.kid ?: ""
-        return when {
-            keyData.d != null -> { // import d as the private key handle
-                CryptoKey(
-                    type = KeyType.Private,
-                    extractable = extractable,
-                    algorithm = algorithm,
-                    usages = keyUsages.toList(),
-                    handle = Secp256k1Handle(
-                        alias,
-                        Base64.decode(stringToByteArray(keyData.d!!), Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
-                    )
-                )
-            }
-            keyData.k != null -> { // import k as the secret key handle
-                CryptoKey(
-                    type = KeyType.Secret,
-                    extractable = extractable,
-                    algorithm = algorithm,
-                    usages = keyUsages.toList(),
-                    handle = Secp256k1Handle(
-                        alias,
-                        Base64.decode(stringToByteArray(keyData.k!!), Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
-                    )
-                )
-            }
-            else -> {// public key
-                val x = Base64.decode(stringToByteArray(keyData.x!!), Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
-                val y = Base64.decode(stringToByteArray(keyData.y!!), Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
-                val xyData = ByteArray(65)
-                xyData[0] = Secp256k1Tag.UNCOMPRESSED.byte
-                x.forEachIndexed { index, byte ->
-                    xyData[index + 1] = byte
-                }
-                y.forEachIndexed { index, byte ->
-                    xyData[index + 33] = byte
-                }
-                CryptoKey(
-                    type = KeyType.Public,
-                    extractable = extractable,
-                    algorithm = algorithm,
-                    usages = keyUsages.toList(),
-                    handle = Secp256k1Handle(alias, xyData)
-                )
-            }
-        }
-    }
-
-    override fun onExportKeyJwk(key: CryptoKey): JsonWebKey {
-        val keyOps = mutableListOf<String>()
-        for (usage in key.usages) {
-            keyOps.add(usage.value)
-        }
-        val publicKey: ByteArray
-        val handle = key.handle as Secp256k1Handle
-        val d: String? = if (key.type == KeyType.Private) {
-            publicKey = generatePublicKeyFromPrivateKey(handle.data)
-            Base64.encodeToString(handle.data, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
-        } else {
-            publicKey = handle.data
-            null
-        }
-        val xyData = publicToXY(publicKey)
-        return JsonWebKey(
-            kty = com.microsoft.did.sdk.crypto.keys.KeyType.EllipticCurve.value,
-            kid = handle.alias,
-            crv = W3cCryptoApiConstants.Secp256k1.value,
-            use = "sig",
-            key_ops = keyOps,
-            alg = JwaCryptoConverter.webCryptoToJwa(key.algorithm),
-            ext = key.extractable,
-            d = d?.trim(),
-            x = xyData.first.trim(),
-            y = xyData.second.trim()
-        )
     }
 
     enum class Secp256k1Tag(val byte: Byte) {
