79b75c4be60c4dfedea32dad63d8f9d6bd6bddb7
Andreas Mikolajewski <anmikola@microsoft.com>
2021-01-27 02:11:51 +0100

    Add test

--- sdk/src/androidTest/java/com/microsoft/did/sdk/crypto/keyStore/EncryptedKeyStoreInstrumentedTest.kt
+++ sdk/src/androidTest/java/com/microsoft/did/sdk/crypto/keyStore/EncryptedKeyStoreInstrumentedTest.kt
@@ -5,141 +5,40 @@
 
 package com.microsoft.did.sdk.crypto.keyStore
 
-import android.content.Context
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
-import com.microsoft.did.sdk.crypto.keys.SecretKey
-import com.microsoft.did.sdk.crypto.keys.rsa.RsaPrivateKey
-import com.microsoft.did.sdk.crypto.keys.rsa.RsaPublicKey
-import com.microsoft.did.sdk.crypto.models.Sha
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.KeyUsage
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.RsaHashedKeyAlgorithm
-import com.microsoft.did.sdk.di.defaultTestSerializer
+import com.microsoft.did.sdk.util.Constants
 import com.microsoft.did.sdk.util.controlflow.KeyStoreException
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.spongycastle.jce.ECNamedCurveTable
+import java.math.BigInteger
+import java.security.KeyFactory
+import java.security.PrivateKey
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class EncryptedKeyStoreInstrumentedTest {
 
     private val keyStore: EncryptedKeyStore
     private val keyRef: String = "TestKeys"
-    private var actualPublicKey: RsaPublicKey
-    private var actualPrivateKey: RsaPrivateKey
+    private var expectedPrivateKey: PrivateKey
 
     init {
-        val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
-        keyStore = EncryptedKeyStore(context, defaultTestSerializer)
-        val androidSubtle = AndroidSubtle(keyStore)
-        val keyPair = androidSubtle.generateKeyPair(
-            RsaHashedKeyAlgorithm(
-                modulusLength = 4096UL,
-                publicExponent = 65537UL,
-                hash = Sha.SHA256.algorithm,
-                additionalParams = mapOf("KeyReference" to keyRef)
-            ), false, listOf(KeyUsage.Encrypt, KeyUsage.Decrypt)
-        )
-        actualPublicKey = RsaPublicKey(androidSubtle.exportKeyJwk(keyPair.publicKey))
-        actualPrivateKey = RsaPrivateKey(androidSubtle.exportKeyJwk(keyPair.privateKey))
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        keyStore = EncryptedKeyStore(context)
+
+        val secret = ByteArray(32) { pos -> pos.toByte() }
+        val keySpec = org.spongycastle.jce.spec.ECPrivateKeySpec(BigInteger(1, secret), ECNamedCurveTable.getParameterSpec(Constants.SECP256K1_CURVE_NAME_EC))
+        expectedPrivateKey = KeyFactory.getInstance("EC", "SC").generatePrivate(keySpec)
     }
 
     @Test
-    fun savePublicAndPrivateKeyPairTest() {
-        keyStore.saveKey(keyRef, actualPublicKey)
-        keyStore.saveKey(keyRef, actualPrivateKey)
-
-        val expectedPublicKeyById = keyStore.getPublicKeyById(actualPublicKey.kid)
-        assertThat(actualPublicKey).isEqualToComparingFieldByFieldRecursively(expectedPublicKeyById)
-
-        val expectedPrivateKeyById = keyStore.getPrivateKeyById(actualPrivateKey.kid)
-        assertThat(actualPrivateKey).isEqualToComparingFieldByFieldRecursively(expectedPrivateKeyById)
-    }
-
-    @Test
-    fun listKeysTest() {
-        keyStore.saveKey(keyRef, actualPrivateKey)
-        keyStore.saveKey(keyRef, actualPublicKey)
-        val keysInKeyStore = keyStore.list()
-        val expectedPrivateKey = keysInKeyStore[keyRef]
-        assertThat(expectedPrivateKey!!.getLatestKeyId()).isNotNull()
-        assertThat(actualPrivateKey.kid).isEqualTo(expectedPrivateKey.kids.firstOrNull())
-        val expectedPublicKey = keysInKeyStore[keyRef]
-        assertThat(actualPublicKey.kid).isEqualTo(expectedPublicKey!!.kids.firstOrNull())
-    }
-
-    @Test
-    fun invalidKeyReferenceTest() {
-        val nonExistingPublicKeyRef = "kid1"
-        assertThatThrownBy { keyStore.getKey(nonExistingPublicKeyRef) }.isInstanceOf(KeyStoreException::class.java)
-    }
-
-    @Test
-    fun getPublicKeyByReferenceTest() {
-        keyStore.saveKey(keyRef, actualPublicKey)
-        val expectedPublicKey = keyStore.getKey(keyRef)
-        assertThat(actualPublicKey.key).isEqualTo(expectedPublicKey.keys[0].jwk)
-    }
-
-    @Test
-    fun getPublicKeyByIdTest() {
-        keyStore.saveKey(keyRef, actualPublicKey)
-        val expectedPublicKey = keyStore.getPublicKeyById(actualPublicKey.kid)
-        assertThat(actualPublicKey).isEqualToComparingFieldByFieldRecursively(expectedPublicKey)
-    }
-
-    @Test
-    fun getPrivateKeyByReferenceTest() {
-        keyStore.saveKey(keyRef, actualPrivateKey)
-        val expectedPrivateKey = keyStore.getPrivateKey(keyRef)
-        assertThat(actualPrivateKey.key).isEqualTo(expectedPrivateKey.keys[0].key)
-    }
-
-    @Test
-    fun getPrivateKeyByIdTest() {
-        keyStore.saveKey(keyRef, actualPrivateKey)
-        val expectedPrivateKey = keyStore.getPrivateKeyById(actualPrivateKey.kid)
-        assertThat(actualPrivateKey).isEqualToComparingFieldByFieldRecursively(expectedPrivateKey)
-    }
-
-    @Test
-    fun checkOrCreateKidTest() {
-        keyStore.saveKey(keyRef, actualPrivateKey)
-        val kid = keyStore.checkOrCreateKeyId(keyRef, null)
-        assertThat(kid).startsWith("#${keyRef}_")
-    }
-
-    @Test
-    fun checkOrCreateKidFirstKeyTest() {
-        val kid = keyStore.checkOrCreateKeyId(keyRef, null)
-        assertThat(kid).startsWith("#${keyRef}_1")
-    }
-
-    @Test
-    fun saveAndGetSecretKeyTest() {
-        val secretKey = SecretKey(
-            JsonWebKey(
-                kty = KeyType.Octets.value,
-                kid = "#${keyRef}_1"
-            )
-        )
-        keyStore.saveKey(keyRef, secretKey)
-        val actualSecretKey = keyStore.getSecretKey(keyRef)
-        assertThat(actualSecretKey.keys.firstOrNull()).isEqualToComparingFieldByFieldRecursively(secretKey)
-    }
-
-    @Test
-    fun getSecretKeyByIdTest() {
-        val secretKey = SecretKey(
-            JsonWebKey(
-                kty = KeyType.Octets.value,
-                kid = "#secret_2"
-            )
-        )
-        keyStore.saveKey("secret", secretKey)
-        val actualSecretKey = keyStore.getSecretKeyById(secretKey.kid)
-        assertThat(actualSecretKey).isEqualToComparingFieldByFieldRecursively(secretKey)
+    fun saveAndGetPrivateKeyTest() {
+        keyStore.saveKey(expectedPrivateKey, keyRef)
+        val actualPrivateKey = keyStore.getKey<PrivateKey>(keyRef)
+        assertThat(expectedPrivateKey).isEqualToComparingFieldByFieldRecursively(actualPrivateKey)
     }
 
 }
