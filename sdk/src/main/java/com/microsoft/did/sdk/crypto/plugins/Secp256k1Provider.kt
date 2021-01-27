b2c9b413b77c2838ec0bb4d41b09925c45231721
Andreas Mikolajewski <anmikola@microsoft.com>
2021-01-27 02:56:27 +0100

    Simplify Provider

--- sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/Secp256k1Provider.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/Secp256k1Provider.kt
@@ -2,27 +2,13 @@
 
 package com.microsoft.did.sdk.crypto.plugins
 
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.CryptoKey
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.Algorithm
-import com.microsoft.did.sdk.crypto.models.webCryptoApi.algorithms.EcdsaParams
+import com.microsoft.did.sdk.crypto.plugins.subtleCrypto.Provider
 import com.microsoft.did.sdk.util.Constants.SECP256K1_CURVE_NAME_EC
-import com.microsoft.did.sdk.util.controlflow.SignatureException
-import com.microsoft.did.sdk.util.convertSignedToUnsignedByteArray
-import org.spongycastle.asn1.ASN1InputStream
-import org.spongycastle.asn1.ASN1Integer
-import org.spongycastle.asn1.DERSequenceGenerator
-import org.spongycastle.asn1.DLSequence
-import org.spongycastle.crypto.params.ECDomainParameters
-import org.spongycastle.crypto.params.ECPublicKeyParameters
-import org.spongycastle.crypto.signers.ECDSASigner
 import org.spongycastle.jce.ECNamedCurveTable
 import org.spongycastle.jce.provider.BouncyCastleProvider
-import java.io.ByteArrayOutputStream
-import java.math.BigInteger
 import java.security.*
-import com.microsoft.did.sdk.crypto.keyStore.KeyStore
 
-class Secp256k1Provider {
+class Secp256k1Provider : Provider() {
     init {
         Security.insertProviderAt(BouncyCastleProvider(), Security.getProviders().size + 1)
     }
@@ -34,90 +20,27 @@ class Secp256k1Provider {
         private const val PROVIDER = "SC"
     }
 
-    fun onGenerateKeyPair(): KeyPair {
+    override fun generateKeyPair(): KeyPair {
         val keyGen = KeyPairGenerator.getInstance(KEY_ALGORITHM, KEY_PROVIDER)
         keyGen.initialize(ECNamedCurveTable.getParameterSpec(SECP256K1_CURVE_NAME_EC))
         return keyGen.genKeyPair()
     }
 
-    fun onSign(data: ByteArray, keyStore: KeyStore, keyId: String): ByteArray {
-        val signingKey = keyStore.getKey<PrivateKey>(keyId)
+    override fun sign(key: PrivateKey, data: ByteArray): ByteArray {
         val signer = Signature.getInstance(SIGNATURE_ALGORITHM, PROVIDER)
                 .apply {
-                    initSign(signingKey)
+                    initSign(key)
                     update(data)
                 }
         return signer.sign()
     }
 
-    override fun onVerify(algorithm: Algorithm, key: CryptoKey, signature: ByteArray, data: ByteArray): Boolean {
-        if (signature.size > 64) {
-            throw SignatureException("Signature is not in R|S format")
-        }
-        val keyData = (key.handle as Secp256k1Handle).data
-        val ecAlgorithm = algorithm as EcdsaParams
-        val hashedData = subtleCryptoSha.digest(ecAlgorithm.hash, data)
-        if (hashedData.size != 32) {
-            throw SignatureException("Data must be 32 bytes")
-        }
-
-        val verifySigner = ECDSASigner()
-        val ecDomainParameters = createCurveParameters()
-        val publicKeyParams = ECPublicKeyParameters(ecDomainParameters.curve.decodePoint(keyData), ecDomainParameters)
-        verifySigner.init(false, publicKeyParams)
-
-        return verifySigner.verifySignature(
-            hashedData,
-            BigInteger(1, signature.sliceArray(0 until signature.size / 2)),
-            BigInteger(1, signature.sliceArray(signature.size / 2 until signature.size))
-        )
-    }
-
-    // NEW CODE
-
-//        val keyGen = KeyPairGenerator.getInstance("EC", "SC")
-//        keyGen.initialize(ECNamedCurveTable.getParameterSpec(SECP256K1_CURVE_NAME_EC))
-//        val keyPair = keyGen.genKeyPair()
-//        val key = keyPair.private
-//        key.encoded
-
-    /**
-     * Signature is returned as array of BigIntegers for R and S. Converting them into unsigned byte array.
-     */
-    private fun convertSignatureToUnsignedByteArray(signature: Array<BigInteger>): ByteArray {
-        val r = convertSignedToUnsignedByteArray(signature[0].toByteArray())
-        val s = convertSignedToUnsignedByteArray(signature[1].toByteArray())
-        return r + s
-    }
-
-    private fun createCurveParameters(): ECDomainParameters {
-        val ecParams = ECNamedCurveTable.getParameterSpec(SECP256K1_CURVE_NAME_EC)
-        return ECDomainParameters(ecParams.curve, ecParams.g, ecParams.n, ecParams.h)
-    }
-
-    //TODO: Incorporate der encoding while adding support for interoperability
-    fun encodeToDer(r: ByteArray, s: ByteArray): ByteArray {
-        val bos = ByteArrayOutputStream(72)
-        val seq = DERSequenceGenerator(bos)
-        seq.addObject(ASN1Integer(r))
-        seq.addObject(ASN1Integer(s))
-        seq.close()
-        return bos.toByteArray()
-    }
-
-    fun decodeFromDER(bytes: ByteArray): Pair<BigInteger, BigInteger> {
-        val decoder = ASN1InputStream(bytes)
-        val seq = decoder.readObject() as DLSequence
-        val r = seq.getObjectAt(0) as ASN1Integer
-        val s = seq.getObjectAt(1) as ASN1Integer
-        return Pair(r.positiveValue, s.positiveValue)
-    }
-
-    enum class Secp256k1Tag(val byte: Byte) {
-        EVEN(0x02),
-        ODD(0x03),
-        UNCOMPRESSED(0x04),
-        HYBRID_EVEN(0x06),
-        HYBRID_ODD(0x07)
+    override fun verify(key: PublicKey, signature: ByteArray, data: ByteArray): Boolean {
+        val verifier = Signature.getInstance(SIGNATURE_ALGORITHM, PROVIDER)
+            .apply {
+                initVerify(key)
+                update(data)
+            }
+        return verifier.verify(signature)
     }
 }
--- sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/subtleCrypto/Provider.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/crypto/plugins/subtleCrypto/Provider.kt
@@ -10,120 +10,38 @@ import com.microsoft.did.sdk.util.controlflow.KeyException
 import com.microsoft.did.sdk.util.controlflow.KeyFormatException
 import com.microsoft.did.sdk.util.controlflow.UnSupportedOperationException
 import com.microsoft.did.sdk.util.controlflow.UnSupportedAlgorithmException
+import java.security.Key
+import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.PublicKey
 import java.util.Locale
 
 abstract class Provider {
-    abstract val name: String
-    abstract val privateKeyUsage: Set<KeyUsage>?
-    abstract val publicKeyUsage: Set<KeyUsage>?
-    abstract val symmetricKeyUsage: Set<KeyUsage>?
-
-    protected open fun onDigest(algorithm: Algorithm, data: ByteArray): ByteArray {
+    protected open fun digest(data: ByteArray): ByteArray {
         throw UnSupportedOperationException("Digest not supported.")
     }
 
-    protected open fun onGenerateKey(algorithm: Algorithm, extractable: Boolean, keyUsages: Set<KeyUsage>): CryptoKey {
+    protected open fun generateKey(): Key {
         throw UnSupportedOperationException("GenerateKey not supported.")
     }
 
-    protected open fun onGenerateKeyPair(algorithm: Algorithm, extractable: Boolean, keyUsages: Set<KeyUsage>): CryptoKeyPair {
+    protected open fun generateKeyPair(): KeyPair {
         throw UnSupportedOperationException("GenerateKeyPair not supported.")
     }
 
-    protected open fun onSign(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
+    protected open fun sign(key: PrivateKey, data: ByteArray): ByteArray {
         throw UnSupportedOperationException("Sign not supported.")
     }
 
-    protected open fun onVerify(algorithm: Algorithm, key: CryptoKey, signature: ByteArray, data: ByteArray): Boolean {
+    protected open fun verify(key: PublicKey, signature: ByteArray, data: ByteArray): Boolean {
         throw UnSupportedOperationException("Verify not supported.")
     }
 
-    protected open fun onEncrypt(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
+    protected open fun encrypt(key: CryptoKey, data: ByteArray): ByteArray {
         throw UnSupportedOperationException("Encrypt not supported.")
     }
 
-    protected open fun onDecrypt(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
+    protected open fun decrypt(key: CryptoKey, data: ByteArray): ByteArray {
         throw UnSupportedOperationException("Decrypt not supported.")
-    }
-
-    fun digest(algorithm: Algorithm, data: ByteArray): ByteArray {
-        checkDigest(algorithm)
-        return this.onDigest(algorithm, data)
-    }
-
-    fun sign(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
-        checkSign(algorithm, key)
-        return onSign(algorithm, key, data)
-    }
-
-    fun verify(algorithm: Algorithm, key: CryptoKey, signature: ByteArray, data: ByteArray): Boolean {
-        checkVerify(algorithm, key)
-        return onVerify(algorithm, key, signature, data)
-    }
-
-    fun encrypt(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
-        checkEncrypt(algorithm, key)
-        return onEncrypt(algorithm, key, data)
-    }
-
-    fun decrypt(algorithm: Algorithm, key: CryptoKey, data: ByteArray): ByteArray {
-        checkDecrypt(algorithm, key)
-        return onDecrypt(algorithm, key, data)
-    }
-
-    private fun checkDigest(algorithm: Algorithm) {
-        checkAlgorithmName(algorithm)
-    }
-
-    private fun checkSign(algorithm: Algorithm, key: CryptoKey) {
-        checkAlgorithmName(algorithm)
-        checkAlgorithmParams(algorithm)
-        checkCryptoKey(key, KeyUsage.Sign)
-    }
-
-    private fun checkVerify(algorithm: Algorithm, key: CryptoKey) {
-        checkAlgorithmName(algorithm)
-        checkAlgorithmParams(algorithm)
-        checkCryptoKey(key, KeyUsage.Verify)
-    }
-
-    private fun checkEncrypt(algorithm: Algorithm, key: CryptoKey) {
-        checkAlgorithmName(algorithm)
-        checkAlgorithmParams(algorithm)
-        checkCryptoKey(key, KeyUsage.Encrypt)
-    }
-
-    private fun checkDecrypt(algorithm: Algorithm, key: CryptoKey) {
-        this.checkAlgorithmName(algorithm)
-        this.checkAlgorithmParams(algorithm)
-        this.checkCryptoKey(key, KeyUsage.Decrypt)
-    }
-
-    protected open fun checkAlgorithmName(algorithm: Algorithm) {
-        if (algorithm.name.toLowerCase(Locale.ENGLISH) != this.name.toLowerCase(Locale.ENGLISH)) {
-            throw UnSupportedAlgorithmException("Unrecognized Algorithm ${algorithm.name}")
-        }
-    }
-
-    protected open fun checkAlgorithmParams(algorithm: Algorithm) {
-        // there are no generic checks to perform
-    }
-
-    private fun checkKeyUsages(usages: Set<KeyUsage>, allowed: Set<KeyUsage>) {
-        val forbiddenUsages = usages - allowed
-        if (forbiddenUsages.isNotEmpty()) {
-            throw KeyException("Key Usages contains forbidden Key Usage: ${forbiddenUsages.joinToString { use -> use.value }}")
-        }
-    }
-
-    open fun checkCryptoKey(key: CryptoKey, keyUsage: KeyUsage) {
-        checkAlgorithmName(key.algorithm)
-        if (!key.usages.contains(keyUsage)) {
-            throw KeyException("Key does not allow ${keyUsage.name}")
-        }
-    }
-
-    protected open fun checkImportParams(algorithm: Algorithm) {
-        // there are no generic checks to perform
     }
 }
