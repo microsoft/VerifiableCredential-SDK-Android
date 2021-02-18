b94830c49326f950cb1649c44045cb9f511939b7
Andreas Mikolajewski <anmikola@microsoft.com>
2021-02-18 18:35:41 +0100

    Make base64 conversion safe

--- sdk/src/main/java/com/microsoft/did/sdk/util/ImageUtil.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/util/ImageUtil.kt
@@ -5,15 +5,22 @@ package com.microsoft.did.sdk.util
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
+import com.microsoft.did.sdk.util.log.SdkLog
 import java.io.ByteArrayOutputStream
 
 object ImageUtil {
-    fun convert(base64Str: String): Bitmap {
-        val decodedBytes = Base64.decode(
-            base64Str.substring(base64Str.indexOf(",") + 1),
-            Base64.DEFAULT
-        )
-        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    fun convert(base64Str: String?): Bitmap? {
+        if (base64Str == null) return null
+        return try {
+            val decodedBytes = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",") + 1),
+                Base64.DEFAULT
+            )
+            BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        } catch (ex: Exception) {
+            SdkLog.w("Image couldn't be converted from Base64", ex)
+            null
+        }
     }
 
     fun convert(bitmap: Bitmap): String {
