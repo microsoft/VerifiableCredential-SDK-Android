d767e3d1a86fcc4552cd5c5fa051514cfa981e28
Andreas Mikolajewski <anmikola@microsoft.com>
2021-03-03 00:46:32 +0100

    Make perf an event

--- sdk/src/main/java/com/microsoft/did/sdk/util/MeasureTime.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/util/MeasureTime.kt
@@ -8,6 +8,6 @@ inline fun <R> logTime(name: String, block: () -> R): R {
     val start = System.currentTimeMillis()
     val result = block()
     val elapsedTime = System.currentTimeMillis() - start
-    SdkLog.i("Perf: $name - ${elapsedTime}ms")
+    SdkLog.event("PerformanceMetrics", mapOf("name" to name, "time" to "${elapsedTime}ms"))
     return result
 }
