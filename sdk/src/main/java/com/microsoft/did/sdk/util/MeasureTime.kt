71b1e695edab01e30a390fdf0d492599e1a49d2c
Andreas Mikolajewski <anmikola@microsoft.com>
2021-03-11 01:16:12 +0100

    Add Network Performance Logging

--- sdk/src/main/java/com/microsoft/did/sdk/util/MeasureTime.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/util/MeasureTime.kt
@@ -2,12 +2,46 @@
 
 package com.microsoft.did.sdk.util
 
+import com.microsoft.did.sdk.util.MetricsConstants.NAME
+import com.microsoft.did.sdk.util.MetricsConstants.TIME
 import com.microsoft.did.sdk.util.log.SdkLog
+import retrofit2.Response
+
+object MetricsConstants {
+    const val NAME = "Name"
+    const val TIME = "Time"
+}
 
 inline fun <R> logTime(name: String, block: () -> R): R {
     val start = System.currentTimeMillis()
     val result = block()
     val elapsedTime = System.currentTimeMillis() - start
-    SdkLog.event("PerformanceMetrics", mapOf("name" to name, "time" to "${elapsedTime}ms"))
+    SdkLog.event(
+        "PerformanceMetrics", mapOf(
+            NAME to name,
+            TIME to "$elapsedTime"
+        )
+    )
+    return result
+}
+
+inline fun <S> logNetworkTime(name: String, block: () -> Response<S>): Response<S> {
+    val start = System.currentTimeMillis()
+    val result = block()
+    val elapsedTime = System.currentTimeMillis() - start
+
+    val cvRequest = result.raw().request().headers()[Constants.CORRELATION_VECTOR_HEADER] ?: "none"
+    val cvResponse = result.raw().headers()[Constants.CORRELATION_VECTOR_HEADER] ?: "none"
+
+    SdkLog.event(
+        "NetworkMetrics", mapOf(
+            NAME to name,
+            TIME to "$elapsedTime",
+            "CV_request" to cvRequest,
+            "CV_response" to cvResponse,
+            "isSuccessful" to "${result.isSuccessful}",
+            "code" to "${result.code()}"
+        )
+    )
     return result
 }
