efa0ebe3f8770216fac8e957d83e93bda9264487
Logan Girvin <logirvin@microsoft.com>
2021-04-21 15:29:18 -0700

    added Jwe Protected Backup Factory tests

--- sdk/src/test/java/com/microsoft/did/sdk/datasource/file/JweProtectedBackupFactoryTest.kt
+++ sdk/src/test/java/com/microsoft/did/sdk/datasource/file/JweProtectedBackupFactoryTest.kt
@@ -2,5 +2,54 @@
 
 package com.microsoft.did.sdk.datasource.file
 
+import com.microsoft.did.sdk.datasource.file.models.VCMetadata
+import com.microsoft.did.sdk.datasource.file.models.WalletMetadata
+import android.util.VerifiableCredentialUtil
+import com.microsoft.did.sdk.datasource.file.models.MicrosoftUnprotectedBackup2020
+import com.microsoft.did.sdk.util.defaultTestSerializer
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class JweProtectedBackupFactoryTest {
+    private val walletMetadata = WalletMetadata()
+
+    private val vcMetadata = VCMetadata(
+        VerifiableCredentialUtil.testDisplayContract
+    )
+
+    private val backup = MicrosoftUnprotectedBackup2020(
+        mapOf("test" to VerifiableCredentialUtil.testVerifiedCredential.raw),
+        mapOf("test" to vcMetadata),
+        walletMetadata,
+        listOf(VerifiableCredentialUtil.rawIdentifier)
+    )
+
+    val jweProtectedBackupFactory = JweProtectedBackupFactory(defaultTestSerializer)
+
+    @Test
+    fun createPasswordBackupTest() {
+        val password = "foobarbaz"
+        val actual = jweProtectedBackupFactory.createPasswordBackup(
+            backup, password
+        )
+        assertTrue(actual.jweToken.contentAsString.isNotBlank())
+    }
+
+    @Test
+    fun parseBackupTest() {
+        val password = "foobarbaz"
+        val backup = jweProtectedBackupFactory.createPasswordBackup(
+            backup, password
+        )
+        val outputData = ByteArrayOutputStream()
+        jweProtectedBackupFactory.writeOutput(backup, outputData)
+        assertTrue(outputData.size() > 0)
+        val inputData = ByteArrayInputStream(outputData.toByteArray())
+        val actual = jweProtectedBackupFactory.parseBackup(inputData)
+        assertEquals(backup.jweToken.serialize(), actual.jweToken.serialize())
+    }
 }
