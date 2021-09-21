95bedc92c30b6de232dc9d63d7e320c399318268
nithyaganeshng <nithyagansh@gmail.com>
2021-09-21 00:37:27 -0700

    revert changes to tests

--- sdk/src/main/java/com/microsoft/did/sdk/LinkedDomainsService.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/LinkedDomainsService.kt
@@ -59,7 +59,7 @@ class LinkedDomainsService @Inject constructor(
         }
     }
 
-    suspend fun getWellKnownConfigDocument(domainUrl: String) = FetchWellKnownConfigDocumentNetworkOperation(
+    private suspend fun getWellKnownConfigDocument(domainUrl: String) = FetchWellKnownConfigDocumentNetworkOperation(
         domainUrl,
         apiProvider
     ).fire()
