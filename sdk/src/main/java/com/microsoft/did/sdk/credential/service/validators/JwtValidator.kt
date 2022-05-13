b304ccdf71969f82fc154ec2d911e73bb76ec488
nithyaganeshng <nithyagansh@gmail.com>
2022-05-13 17:49:18 -0400

    verify using the public key in jws

--- sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/JwtValidator.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/JwtValidator.kt
@@ -26,40 +26,30 @@ class JwtValidator @Inject constructor(
      * Verify the signature on the JwsToken.
      */
     suspend fun verifySignature(token: JwsToken): Boolean {
-        val (didInHeader: String?, keyIdInHeader: String) = token.keyId?.let { JwaCryptoHelper.extractDidAndKeyId(it) } ?: throw ValidatorException("key id is missing in jwt")
-        val publicKeys = didInHeader?.let { retrievePublicKey(didInHeader, keyIdInHeader) } ?: throw ValidatorException("DID is missing in jwt header")
-        if (publicKeys.isNullOrEmpty()) throw ValidatorException("No public key found in identifier document")
+        val (didInHeader: String?, keyIdInHeader: String) = getDidAndKeyIdFromHeader(token)
+        if (didInHeader == null) throw ValidatorException("JWS contains no DID")
+        val publicKeys = resolvePublicKey(didInHeader, keyIdInHeader)
         return token.verify(publicKeys)
     }
 
     fun validateDidInHeaderAndPayload(jwsToken: JwsToken, didInPayload: String): Boolean {
-        val didInHeader = getDidFromHeader(jwsToken)
-        didInHeader ?: throw ValidatorException("JWS contains no DID")
+        val didInHeader = getDidAndKeyIdFromHeader(jwsToken).first ?: throw ValidatorException("JWS contains no DID")
         return didInHeader == didInPayload
     }
 
-    private fun getDidAndKeyIdFromHeader(token: JwsToken): Pair<String, String> {
-        token.keyId?.let { kid ->
-            val parsedKid = kid.split("#")
-            return Pair(parsedKid[0], parsedKid[1])
-        }
+    private fun getDidAndKeyIdFromHeader(token: JwsToken): Pair<String?, String> {
+        token.keyId?.let { kid -> return JwaCryptoHelper.extractDidAndKeyId(kid) }
         throw ValidatorException("JWS contains no key id")
     }
 
-    private suspend fun retrievePublicKey(did: String, keyId: String): List<PublicKey> {
+    private suspend fun resolvePublicKey(did: String, keyId: String): List<PublicKey> {
         return when (val requesterDidDocument = resolver.resolve(did)) {
             is Result.Success -> {
                 val publicKeys = requesterDidDocument.payload.verificationMethod
                 if (publicKeys.isNullOrEmpty()) throw ValidatorException("No public key found in identifier document")
-                publicKeys.filter { JwaCryptoHelper.extractDidAndKeyId(it.id).second == keyId }.map { it.toPublicKey() }
+                publicKeys.filter { publicKey -> JwaCryptoHelper.extractDidAndKeyId(publicKey.id).second == keyId }.map { it.toPublicKey() }
             }
             is Result.Failure -> throw ValidatorException("Unable to fetch public keys", requesterDidDocument.payload)
         }
-    }
-
-    private fun getDidFromHeader(token: JwsToken): String? {
-        token.keyId ?: throw ValidatorException("JWS contains no key id")
-        val (didInHeader, _) = JwaCryptoHelper.extractDidAndKeyId(token.keyId!!)
-        return didInHeader
     }
 }
