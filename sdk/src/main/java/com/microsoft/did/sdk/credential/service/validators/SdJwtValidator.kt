6bdf070faf6931830195913aa82358a3b3c6551a
Andreas Miko <anmikola@microsoft.com>
2022-12-15 01:48:29 +0100

    Add methods to decode disclosure

--- sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/SdJwtValidator.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/credential/service/validators/SdJwtValidator.kt
@@ -18,12 +18,14 @@ import javax.inject.Singleton
 @Singleton
 class SdJwtValidator @Inject constructor() {
 
+    class Disclosure(val claimName: String, val claimValue: String)
+
     private val serializer = Json {
         ignoreUnknownKeys = true
         isLenient = true
     }
 
-    suspend fun verifyDisclosures(contents: VerifiableCredentialContent, disclosures: List<String>?): Boolean {
+    fun verifyDisclosures(contents: VerifiableCredentialContent, disclosures: List<String>?): Boolean {
         if (disclosures == null) return true
         if (contents.sd_digest_derivation_alg != "sha-256") throw AlgorithmException("Unknown digest algorithm. Only 'sha-256' is supported at this time")
         val sdHashes = getSdSet(contents)
@@ -43,5 +45,11 @@ class SdJwtValidator @Inject constructor() {
             sdSet.add(jsonValue.jsonPrimitive.content)
         }
         return sdSet
+    }
+
+    fun decodeDisclosure(b64Disclosure: String): Disclosure {
+        val disclosureString = Base64.decode(b64Disclosure, Constants.BASE64_URL_SAFE).decodeToString()
+        val jsonArray = serializer.decodeFromString(JsonArray.serializer(), disclosureString)
+        return Disclosure(jsonArray[1].jsonPrimitive.content, jsonArray[2].jsonPrimitive.content)
     }
 }
