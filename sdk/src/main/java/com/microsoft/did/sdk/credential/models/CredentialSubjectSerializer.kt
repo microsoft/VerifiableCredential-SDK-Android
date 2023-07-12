5e022d688a0efcddda2d149f1c3ff7ac235ed380
Nithya Ganesh <niganesh@microsoft.com>
2023-07-12 14:41:31 -0400

    reverted changes to serialize empty string in claims

--- sdk/src/main/java/com/microsoft/did/sdk/credential/models/CredentialSubjectSerializer.kt
+++ sdk/src/main/java/com/microsoft/did/sdk/credential/models/CredentialSubjectSerializer.kt
@@ -42,12 +42,7 @@ object CredentialSubjectSerializer :
         if (element !is JsonObject) throw SerializationException("CredentialSubject has to be a JSON object")
         val newContent = HashMap<String, JsonElement>()
         element.entries.forEach { entry ->
-            run {
-                if (entry.value.jsonPrimitive.content.isEmpty()) {
-                    newContent[entry.key] = JsonPrimitive("")
-                } else
-                    newContent[entry.key] = serializer.decodeFromString(JsonElement.serializer(), entry.value.jsonPrimitive.content)
-            }
+            newContent[entry.key] = serializer.decodeFromString(JsonElement.serializer(), entry.value.jsonPrimitive.content)
         }
         return JsonObject(newContent)
     }
